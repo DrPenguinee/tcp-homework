d89ab43d61c25a6f47e5a00599774db63436ee0d
kirill <zubrilinkv@yandex.ru>
2023-12-08 20:31:48 +0300

    start commit

---
+++ hw1/TCP_packet.py
@@ -0,0 +1,48 @@
+import struct
+
+
+class TCP_packet:
+    HEADER_SIZE = 13
+    MSS = 10
+    # maximum segment size; the max of the data
+    # HEADER_FORMAT explanation:
+    # seq_num == 4 bytes, ack_num == 4 bytes, header_len == 2 bytes, ACK == 1 byte, SYN == 1 byte, FIN == 1 byte,
+    HEADER_FORMAT = '2IH3b' + str(MSS) + 's'
+    PACKET_SIZE = HEADER_SIZE + MSS
+
+    def __init__(self, seq_num: int, ack_num: int, ACK: int, SYN: int, FIN: int, data: bytes):
+        self.seq_num = seq_num
+        self.ack_num = ack_num
+        self.ACK = ACK
+        self.SYN = SYN
+        self.FIN = FIN
+        self.data = data
+
+    def is_syn(self) -> bool:
+        return self.ACK == 0 and self.SYN == 1 and self.FIN == 0
+
+    def is_synack(self) -> bool:
+        return self.ACK == 1 and self.SYN == 1 and self.FIN == 0
+
+    def is_ack(self) -> bool:
+        return self.ACK == 1
+
+    def is_fin(self) -> bool:
+        return self.ACK == 0 and self.SYN == 0 and self.FIN == 1
+
+    def pack_segment(self) -> bytes:
+        empty_size = self.MSS - len(self.data)
+        self.data += b' ' * empty_size
+
+        return struct.pack(self.HEADER_FORMAT,
+                           self.seq_num, self.ack_num,
+                           self.HEADER_SIZE,
+                           self.ACK, self.SYN, self.FIN,
+                           self.data)
+
+    @classmethod
+    def unpack_segment(cls, packed_segment):
+        (seq_num, ack_num, header_size, ACK, SYN, FIN, data) = struct.unpack(TCP_packet.HEADER_FORMAT, packed_segment)
+        data = data.rstrip(b' ')
+        x = cls(seq_num, ack_num, ACK, SYN, FIN, data)
+        return x
---
+++ hw1/servers.py
@@ -0,0 +1,45 @@
+import os
+import random
+import string
+import time
+
+from protocol import MyTCPProtocol
+
+
+class Base:
+    def __init__(self, socket: MyTCPProtocol, iterations: int, msg_size: int):
+        self.socket = socket
+        self.iterations = iterations
+        self.msg_size = msg_size
+
+
+class EchoServer(Base):
+
+    def run(self):
+        start_time = time.time()
+        for i in range(self.iterations):
+            print("--- %s seconds ---" % (time.time() - start_time))
+            print("RUN NEW SERVER ITERATION: " + str(i))
+            print("SERVER receiving")
+            msg = self.socket.recv(self.msg_size, "server " + str(i))
+            print("SERVER sending")
+            self.socket.send(msg, "server " + str(i))
+            
+class EchoClient(Base):
+
+    def run(self):
+        start_time = time.time()
+        for i in range(self.iterations):
+            print("--- %s seconds ---" % (time.time() - start_time))
+            print("RUN NEW CLIENT ITERATION: " + str(i))
+            # msg = os.urandom(self.msg_size * 2)
+            # msg = msg.strip()[:self.msg_size]
+            characters = string.ascii_letters + string.digits
+            gen_string = ''.join(random.choice(characters) for _ in range(self.msg_size))
+            msg = gen_string.encode()
+
+            print("CLIENT sending")
+            n = self.socket.send(msg, "client " + str(i))
+            assert n == self.msg_size
+            print("CLIENT receiving")
+            assert msg == self.socket.recv(n, "client " + str(i))
