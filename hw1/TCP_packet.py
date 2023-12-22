b936a4f6f559b17813d98ae8f76d112df95b380d
k.zubrilin <k.zubrilin@tinkoff.ru>
2023-12-22 21:42:42 +0300

    send data size

--- hw1/TCP_packet.py
+++ hw1/TCP_packet.py
@@ -2,12 +2,13 @@ import struct
 
 
 class TCP_packet:
-    HEADER_SIZE = 13
+    HEADER_SIZE = 15
     MSS = 10
-    # maximum segment size; the max of the data
-    # HEADER_FORMAT explanation:
-    # seq_num == 4 bytes, ack_num == 4 bytes, header_len == 2 bytes, ACK == 1 byte, SYN == 1 byte, FIN == 1 byte,
-    HEADER_FORMAT = '2IH3b' + str(MSS) + 's'
+    # maximum segment size; the max of the data HEADER_FORMAT explanation:
+    # seq_num == 4 bytes, ack_num == 4 bytes,
+    # header_len == 2 bytes, data_size == 2 bytes,
+    # ACK == 1 byte, SYN == 1 byte, FIN == 1 byte,
+    HEADER_FORMAT = '2I2H3b' + str(MSS) + 's'
     PACKET_SIZE = HEADER_SIZE + MSS
 
     def __init__(self, seq_num: int, ack_num: int, ACK: int, SYN: int, FIN: int, data: bytes):
@@ -31,18 +32,19 @@ class TCP_packet:
         return self.ACK == 0 and self.SYN == 0 and self.FIN == 1
 
     def pack_segment(self) -> bytes:
-        empty_size = self.MSS - len(self.data)
-        self.data += b' ' * empty_size
+        data_size = len(self.data)
+        empty_size = self.MSS - data_size
+        data = self.data + b' ' * empty_size
 
         return struct.pack(self.HEADER_FORMAT,
                            self.seq_num, self.ack_num,
-                           self.HEADER_SIZE,
+                           self.HEADER_SIZE, data_size,
                            self.ACK, self.SYN, self.FIN,
-                           self.data)
+                           data)
 
     @classmethod
     def unpack_segment(cls, packed_segment):
-        (seq_num, ack_num, header_size, ACK, SYN, FIN, data) = struct.unpack(TCP_packet.HEADER_FORMAT, packed_segment)
-        data = data.rstrip(b' ')
+        (seq_num, ack_num, header_size, data_size, ACK, SYN, FIN, data) = struct.unpack(TCP_packet.HEADER_FORMAT, packed_segment)
+        data = data[:data_size]
         x = cls(seq_num, ack_num, ACK, SYN, FIN, data)
         return x
